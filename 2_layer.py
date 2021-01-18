bd684e382b3397e8779ed284cb889dde3b582c92
Nikhil Kunde <nikhil1231@hotmail.co.uk>
2021-01-18 17:31:46 +0000

    non-convex 2

--- 2_layer.py
+++ 2_layer.py
@@ -2,30 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-rand = np.random.normal(0, 1, (2, 2))
-
-a = [rand[0,0], rand[0,1], rand[1,1]]
-
-sax = lambda a, x: np.matmul([[a[0], a[1]], [a[1], a[2]]], x)
 sigmoid = lambda x: 1.0 / (1.0 + np.exp(-x))
 
+def diag(a):
+  return np.array([[a[0], a[1]], [a[1], a[2]]])
+
+def forward(x, w1, w2):
+  out = diag(w1) @ x
+  out = diag(w2) @ sigmoid(out)
+  return out
+
 X = np.random.normal(0, 1, (2, 10))
-Y = sax(a, X)
+Y = forward(X, np.random.normal(0, 1, 3), np.random.normal(0, 1, 3))
 
-def loss(a, b, X, Y):
+def loss(y_hat, Y):
+  return sum(((y_hat - Y) ** 2).flatten())
 
-  out = sax(a, X)
-  out = sigmoid(sax(b, out))
-
-  return sum(((out - Y) ** 2).flatten())
-
-def calc_loss(i, j):
-  return loss([i, j, a[2]], [i, j, a[2]], X, Y)
+def calc_loss(i, j, fixed):
+  y_hat = forward(X, [i, j, fixed[0]], [fixed[1], fixed[2], fixed[3]])
+  return loss(y_hat, Y)
 
 def create_landscape(axis):
-  return [[calc_loss(i, j) for j in axis] for i in axis]
+  rand = np.random.normal(0, 1, 4)
+  return [[calc_loss(i, j, rand) for j in axis] for i in axis]
 
-axis = np.arange(-5, 5, 0.25)
+axis = np.arange(-10, 10, 0.25)
 
 def plot():
   plt.contour(axis, axis, create_landscape(axis), levels=20)
@@ -38,8 +39,7 @@ def plot_3d():
   Z = np.array(create_landscape(axis))
   axis_x, axis_y = np.meshgrid(axis, axis)
 
-  ax.plot_surface(axis_x, axis_y, Z, cmap=cm.Spectral_r,
-                        linewidth=0, antialiased=False)
+  ax.plot_surface(axis_x, axis_y, Z, cmap=cm.Spectral_r, linewidth=0)
   plt.show()
 
 # plot()

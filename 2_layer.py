9ba154b796a210b1c4bab15f4c7b0206b671bb72
Nikhil Kunde <nikhil1231@hotmail.co.uk>
2021-01-18 20:54:30 +0000

    Fixed Y and init weights to same values (removes non-convexity)

--- 2_layer.py
+++ 2_layer.py
@@ -3,11 +3,16 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 
 # 4, 6, 14
-np.random.seed(14)
+np.random.seed(10)
 
 WEIGHTS_DIST = 'first'
 
 sigmoid = lambda x: 1.0 / (1.0 + np.exp(-x))
+
+class TwoLayerNet:
+  def __init__(self, w1, w2):
+    self.w1 = w1
+    self.w2 = w2
 
 def diag(a):
   return np.array([[a[0], a[1]], [a[1], a[2]]])
@@ -34,21 +39,20 @@ def calc_loss(i, j, fixed, Y):
   y_hat = forward(X, form_weights(i, j, fixed, WEIGHTS_DIST))
   return loss(y_hat, Y)
 
-def create_landscape(axis, Y):
-  rand = np.random.normal(0, 1, 4)
-  return [[calc_loss(i, j, rand, Y) for j in axis] for i in axis]
+def create_landscape(axis, fixed, Y):
+  return [[calc_loss(i, j, fixed, Y) for j in axis] for i in axis]
 
 axis = np.arange(-10, 10, 0.25)
 
-def plot(Y):
-  plt.contour(axis, axis, create_landscape(axis, Y), levels=20)
+def plot(fixed, Y):
+  plt.contour(axis, axis, create_landscape(axis, fixed, Y), levels=20)
   plt.show()
 
-def plot_3d(Y):
+def plot_3d(fixed, Y):
   fig = plt.figure()
   ax = fig.gca(projection='3d')
 
-  Z = np.array(create_landscape(axis, Y))
+  Z = np.array(create_landscape(axis, fixed, Y))
   axis_x, axis_y = np.meshgrid(axis, axis)
 
   ax.plot_surface(axis_x, axis_y, Z, cmap=cm.Spectral_r, linewidth=0)
@@ -56,9 +60,14 @@ def plot_3d(Y):
 
 if __name__ == "__main__":
   rand = np.random.normal(0, 1, 6)
+  fixed = rand[2:]
 
   X = np.random.normal(0, 1, (2, 10))
-  Y = forward(X, form_weights(rand[0], rand[1], rand[2:]))
+  Y = forward(X, form_weights(rand[0], rand[1], fixed))
 
-  # plot(Y)
-  plot_3d(Y)
+  rand_init = np.random.normal(0, 1, 2)
+  model = TwoLayerNet(*form_weights(*rand_init, fixed, WEIGHTS_DIST))
+
+  # Fixed weights set to same as Y, change to random to achieve non-convexity
+  # plot(fixed, Y)
+  plot_3d(fixed, Y)

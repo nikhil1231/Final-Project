367b66f9f467cb04aa716e7f811a3202e9f5a9dd
Nikhil Kunde <nikhil1231@hotmail.co.uk>
2021-01-18 18:01:10 +0000

    Refactor, added seed

--- 2_layer.py
+++ 2_layer.py
@@ -2,45 +2,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
+# 4, 6, 14
+np.random.seed(14)
+
+WEIGHTS_DIST = 'first'
+
 sigmoid = lambda x: 1.0 / (1.0 + np.exp(-x))
 
 def diag(a):
   return np.array([[a[0], a[1]], [a[1], a[2]]])
 
-def forward(x, w1, w2):
-  out = diag(w1) @ x
-  out = diag(w2) @ sigmoid(out)
-  return out
+def form_weights(i, j, fixed, dist='first'):
+  weights = None
+  if dist == 'first':
+    weights = [i, j, fixed[0]], [fixed[1], fixed[2], fixed[3]]
+  elif dist == 'second':
+    # This does not result in non-convexity
+    weights = [fixed[0], fixed[1], fixed[2]], [i, j, fixed[3]]
 
-X = np.random.normal(0, 1, (2, 10))
-Y = forward(X, np.random.normal(0, 1, 3), np.random.normal(0, 1, 3))
+  return list(map(lambda x: diag(x), weights))
+
+def forward(x, w):
+  out = w[0] @ x
+  out = w[1] @ sigmoid(out)
+  return out
 
 def loss(y_hat, Y):
   return sum(((y_hat - Y) ** 2).flatten())
 
-def calc_loss(i, j, fixed):
-  y_hat = forward(X, [i, j, fixed[0]], [fixed[1], fixed[2], fixed[3]])
+def calc_loss(i, j, fixed, Y):
+  y_hat = forward(X, form_weights(i, j, fixed, WEIGHTS_DIST))
   return loss(y_hat, Y)
 
-def create_landscape(axis):
+def create_landscape(axis, Y):
   rand = np.random.normal(0, 1, 4)
-  return [[calc_loss(i, j, rand) for j in axis] for i in axis]
+  return [[calc_loss(i, j, rand, Y) for j in axis] for i in axis]
 
 axis = np.arange(-10, 10, 0.25)
 
-def plot():
-  plt.contour(axis, axis, create_landscape(axis), levels=20)
+def plot(Y):
+  plt.contour(axis, axis, create_landscape(axis, Y), levels=20)
   plt.show()
 
-def plot_3d():
+def plot_3d(Y):
   fig = plt.figure()
   ax = fig.gca(projection='3d')
 
-  Z = np.array(create_landscape(axis))
+  Z = np.array(create_landscape(axis, Y))
   axis_x, axis_y = np.meshgrid(axis, axis)
 
   ax.plot_surface(axis_x, axis_y, Z, cmap=cm.Spectral_r, linewidth=0)
   plt.show()
 
-# plot()
-plot_3d()
+if __name__ == "__main__":
+  rand = np.random.normal(0, 1, 6)
+
+  X = np.random.normal(0, 1, (2, 10))
+  Y = forward(X, form_weights(rand[0], rand[1], rand[2:]))
+
+  # plot(Y)
+  plot_3d(Y)

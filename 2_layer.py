f699614e3a38ac286b343563f9fd5a83e499c2fd
Nikhil Kunde <nikhil1231@hotmail.co.uk>
2021-01-21 14:35:36 +0000

    Added multiple paths. 2D version still bugged I think?

--- 2_layer.py
+++ 2_layer.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-# 4, 6, 14
-# np.random.seed(10)
+np.random.seed(6)
 
 WEIGHTS_DIST = 'first'
-AXIS_SIZE = 10
+MODEL_FIXED_SAME = False
+AXIS_SIZE = 15
 
 sigmoid = lambda x: 1.0 / (1.0 + np.exp(-x))
 
@@ -36,7 +36,9 @@ class TwoLayerNet:
       self.w2 -= lr * dw2 / len(x)
 
     # Need to make sure these are the same?
-    self.w1[0,1] = self.w1[1,0]
+    diff = self.w1[0,1] - self.w1[1,0]
+    self.w1[0,1] -= diff / 2
+    self.w1[1,0] += diff / 2
 
 
 def diag(a):
@@ -84,15 +86,20 @@ def create_landscape(axis, fixed, Y):
 
 axis = np.arange(-AXIS_SIZE, AXIS_SIZE, AXIS_SIZE/100)
 
-def plot(fixed, Y, path=None):
-  plt.contour(axis, axis, create_landscape(axis, fixed, Y), levels=20)
-  if path:
-    params = list(zip(*path))
-    plt.plot(*params[:2], color='red')
-    plt.scatter(*path[-1][:2], color='red', marker='o')
+def plot(fixed, Y, paths=None):
+  plt.contour(axis, axis, create_landscape(axis, fixed, Y), levels=20, cmap=cm.terrain_r)
+  if paths:
+    for path in paths:
+      params = list(zip(*path))
+      xs, ys = params[1], params[0]
+      plt.plot(xs, ys, color='red')
+      plt.scatter(xs[-1], ys[-1], color='red', marker='o')
+
+  plt.xlim([-AXIS_SIZE, AXIS_SIZE])
+  plt.ylim([-AXIS_SIZE, AXIS_SIZE])
   plt.show()
 
-def plot_3d(fixed, Y, path=None):
+def plot_3d(fixed, Y, paths=None):
   fig = plt.figure()
   ax = fig.gca(projection='3d')
 
@@ -100,9 +107,13 @@ def plot_3d(fixed, Y, path=None):
   axis_x, axis_y = np.meshgrid(axis, axis)
 
   ax.plot_surface(axis_x, axis_y, Z, cmap=cm.terrain, linewidth=0, alpha=0.7)
-  if path:
-    ax.plot(*zip(*path), color='red')
-    ax.scatter(*path[-1], marker='o', color='red')
+  if paths:
+    for path in paths:
+      ax.plot(*zip(*path), color='red')
+      ax.scatter(*path[-1], marker='o', color='red')
+
+  plt.xlim([-AXIS_SIZE, AXIS_SIZE])
+  plt.ylim([-AXIS_SIZE, AXIS_SIZE])
   plt.show()
 
 def plot_losses(losses, epochs):
@@ -117,14 +128,21 @@ if __name__ == "__main__":
   X = np.random.normal(0, 1, (2, 10))
   Y = forward(X, form_weights(rand[0], rand[1], fixed))
 
-  rand_init = (np.random.rand(2) * 2 - 1) * AXIS_SIZE
-  model = TwoLayerNet(*form_weights(*rand_init, fixed, WEIGHTS_DIST))
+  epochs = 20000
+  lr = 0.1
 
-  epochs = 10000
+  num_paths = 3
+  sgd_paths = []
 
-  sgd_path, losses = train(epochs, model, X, Y, 0.1)
+  # Model fixed weights set to same as Y, change to random to achieve non-convexity
+  model_fixed = fixed if MODEL_FIXED_SAME else np.random.normal(0, 1, 4)
 
-  # Fixed weights set to same as Y, change to random to achieve non-convexity
+  for _ in range(num_paths):
+    rand_init = (np.random.rand(2) * 2 - 1) * AXIS_SIZE
+    model = TwoLayerNet(*form_weights(*rand_init, model_fixed, WEIGHTS_DIST))
+
+    sgd_paths.append(train(epochs, model, X, Y, lr)[0])
+
   # plot_losses(losses, epochs)
-  plot(fixed, Y, sgd_path)
-  plot_3d(fixed, Y, sgd_path)
+  plot(fixed, Y, sgd_paths)
+  plot_3d(model_fixed, Y, sgd_paths)

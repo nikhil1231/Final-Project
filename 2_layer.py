6a5724edd3cf26f6b8ffd66dc2df7af47a0a1c17
Nikhil Kunde <nikhil1231@hotmail.co.uk>
2021-02-09 16:21:44 +0000

    Added extra layer to second and equal, generalised backprop

--- 2_layer.py
+++ 2_layer.py
@@ -2,70 +2,93 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-# 6
-np.random.seed(6)
+# 6, 8
+# np.random.seed(1)
 
-WEIGHTS_DIST = 'first'
-MODEL_FIXED_SAME = False
+WEIGHTS_DIST = 'equal'
+MODEL_FIXED_SAME = True
 AXIS_SIZE = 15
 
+parameter_positions = {
+  'first': [(0, 0, 0), (0, 0, 1)],
+  'second': [(1, 0, 0), (1, 0, 1)],
+  'equal': [(0, 0, 1), (1, 0, 1)],
+}
+
 sigmoid = lambda x: 1.0 / (1.0 + np.exp(-x))
+inv_sigmoid = lambda y: np.log(y / (1.0 - y))
 
 class TwoLayerNet:
-  def __init__(self, w1, w2):
-    self.w1 = w1
-    self.w2 = w2
+  def __init__(self, w0, w1, w2=np.array([None])):
+    self.w = [w0, w1, w2]
 
   def forward(self, x):
-    self.a1 = sigmoid(self.w1 @ x)
-    self.a2 = self.w2 @ self.a1
+    self.a1 = sigmoid(self.w[0] @ x)
+    self.a2 = self.w[1] @ self.a1
+    if self.w[2].any() != None:
+      self.a3 = self.w[2] @ sigmoid(self.a2)
+      return self.a3
     return self.a2
 
   def backward(self, x, y, lr):
+
+    dw = [None] * 2
+
+    # w[2] will always be fixed, so we can "remove" it before calculating grads
+    if self.w[2].any() != None:
+      inv = np.linalg.inv(self.w[2])
+      y = inv_sigmoid(inv @ y)
+
     error = self.a2 - y
 
-    dw2 = error @ self.a1.T
-    dw1 = self.w2.T @ error * self.a1 * (1 - self.a1) @ x.T
+    dw[1] = error @ self.a1.T
+    dw[0] = self.w[1].T @ error * self.a1 * (1 - self.a1) @ x.T
 
-    # These values are always fixed, so set grad to 0
-    dw1[1,1] = 0
-    dw2[1,1] = 0
+    # Only update weights for non-fixed parameters
+    if WEIGHTS_DIST != 'rotational':
+      pos = parameter_positions[WEIGHTS_DIST]
+      for _pos in pos:
+        self.w[_pos[0]][_pos[1], _pos[2]] -= lr * dw[_pos[0]][_pos[1], _pos[2]] / len(x)
 
-    if WEIGHTS_DIST == 'first':
-      self.w1 -= lr * dw1 / len(x)
-    elif WEIGHTS_DIST == 'second':
-      self.w2 -= lr * dw2 / len(x)
+        # If the same parameter appears twice on the diagonal, fix them to be the same
+        if _pos[1:3] == (0, 1):
+          self.w[_pos[0]][1, 0] -= lr * dw[_pos[0]][1, 0] / len(x)
 
-    # Need to make sure these are the same?
-    diff = self.w1[0,1] - self.w1[1,0]
-    self.w1[0,1] -= diff / 2
-    self.w1[1,0] += diff / 2
+          diff = self.w[_pos[0]][0, 1] - self.w[_pos[0]][1, 0]
+          self.w[_pos[0]][0, 1] -= diff / 2
+          self.w[_pos[0]][1, 0] += diff / 2
 
 
 def diag(a):
   return np.array([[a[0], a[1]], [a[1], a[2]]])
 
+def forward_diag(a):
+  return np.array([[a[0], a[1]], [a[2], a[0]]])
+
 '''
-  Distribute parameters i and j into a pair of 2x2 matrices, to form the weights.
+  Distribute parameters i and j into 2x2 matrices, to form the weights.
 '''
-def form_weights(i, j, fixed, dist='first'):
-  weights = None
-  if dist == 'first':
-    weights = [i, j, fixed[0]], [fixed[1], fixed[2], fixed[3]]
-  elif dist == 'second':
-    # This does not result in non-convexity
-    weights = [fixed[0], fixed[1], fixed[2]], [i, j, fixed[3]]
-  elif dist == 'equal':
-    # weights = [i, fixed[0], fixed[1]], [j, fixed[2], fixed[3]]
-    weights = [fixed[0], i, fixed[1]], [fixed[2], j, fixed[3]]
-  elif dist == 'rotational':
-    weights = [0, i, 0], [0, j, 0]
+def form_weights(i, j, fixed):
+  weights = [[fixed[0], fixed[1], fixed[2]], [fixed[3], fixed[4], fixed[5]]]
+
+  if WEIGHTS_DIST == 'rotational':
+    weights = [np.cos(i), -np.sin(i), np.sin(i)], [np.cos(j), -np.sin(j), np.sin(j)]
+    return list(map(lambda x: forward_diag(x), weights))
+  else:
+    pos = parameter_positions[WEIGHTS_DIST]
+    weights[pos[0][0]][pos[0][1] + pos[0][2]] = i
+    weights[pos[1][0]][pos[1][1] + pos[1][2]] = j
+
+    if WEIGHTS_DIST == 'equal' or WEIGHTS_DIST == 'second':
+      weights += [[fixed[6], fixed[7], fixed[8]]]
 
   return list(map(lambda x: diag(x), weights))
 
 def forward(x, w):
   out = w[0] @ x
   out = w[1] @ sigmoid(out)
+  if len(w) > 2:
+    out = w[2] @ sigmoid(out)
   return out
 
 def loss(y_hat, Y):
@@ -82,12 +105,13 @@ def train(epochs, m, X, Y, lr):
 
     m.backward(X, Y, lr)
 
-    sgd_path.append((m.w1[0,0], m.w1[0,1], _loss))
+    pos = parameter_positions[WEIGHTS_DIST]
+    sgd_path.append((m.w[pos[0][0]][pos[0][1], [pos[0][2]]], m.w[pos[1][0]][pos[1][1], [pos[1][2]]], _loss))
 
   return sgd_path, losses
 
 def calc_loss(i, j, fixed, Y):
-  y_hat = forward(X, form_weights(i, j, fixed, WEIGHTS_DIST))
+  y_hat = forward(X, form_weights(i, j, fixed))
   return loss(y_hat, Y)
 
 def create_landscape(axis, fixed, Y):
@@ -141,27 +165,29 @@ def plot_losses(losses, epochs):
   plt.show()
 
 if __name__ == "__main__":
-  rand = np.random.normal(0, 1, 6)
+  rand = np.random.normal(0, 1, 12)
   fixed = rand[2:]
 
   X = np.random.normal(0, 1, (2, 10))
   Y = forward(X, form_weights(rand[0], rand[1], fixed))
 
-  epochs = 10000
+  # fixed batch vs dist, range of values, orthoganol matices, adding another layer to 'equal' dist.
+  epochs = 50000
   lr = 0.1
 
   num_paths = 3
   sgd_paths = []
 
   # Model fixed weights set to same as Y, change to random to achieve non-convexity
-  model_fixed = fixed if MODEL_FIXED_SAME else np.random.normal(0, 1, 4)
+  model_fixed = fixed if MODEL_FIXED_SAME else np.random.normal(0, 1, 12)
 
   for _ in range(num_paths):
     rand_init = (np.random.rand(2) * 2 - 1) * AXIS_SIZE
-    model = TwoLayerNet(*form_weights(*rand_init, model_fixed, WEIGHTS_DIST))
+    model = TwoLayerNet(*form_weights(*rand_init, model_fixed))
 
     sgd_paths.append(train(epochs, model, X, Y, lr)[0])
 
   # plot_losses(losses, epochs)
-  plot(model_fixed, Y, sgd_paths)
+  # plot(model_fixed, Y, sgd_paths)
+  # plot_3d(model_fixed, Y)
   plot_3d(model_fixed, Y, sgd_paths)

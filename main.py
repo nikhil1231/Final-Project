aa6ebbfcca4ad4a0ca8937b57757c9cff973010a
Nikhil Kunde <nikhil1231@hotmail.co.uk>
2021-03-16 20:00:31 +0000

    Added resnet

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ np.random.seed(6)
 NUM_SAMPLES = 100
 BATCH_SIZE = 10
 
-WEIGHTS_DIST = 'first'
+WEIGHTS_DIST = 'rotational'
 MODEL_FIXED_SAME = True
 
 RAND_SD = 2
 RAND_DIST = 'uniform'
-AXIS_SIZE = 5
+AXIS_SIZE = 15
 
 ADD_NOISE = False
 NOISE_STRENGTH = 5
@@ -27,6 +27,7 @@ parameter_positions = {
   'equal': [(0, 0, 1), (1, 0, 1)],
   'rotational': [(0, 0, 0), (1, 0, 0)],
   'skew': [(0, 0, 1), (1, 0, 1)],
+  'resnet': [(0, 0, 0), (0, 0, 1)],
 }
 
 sigmoid = lambda x: 1.0 / (1.0 + np.exp(-x))
@@ -134,6 +135,64 @@ class RotationalNet:
     self.w = self.form_shell_weights(self.i, self.j)
     self._w = form_weights(self.i, self.j, [0]*6)
 
+class ResNet:
+  def __init__(self, w):
+    self.w = w
+    self.is_3_layer = len(w) == 3
+
+  def forward(self, x):
+    if self.is_3_layer:
+      self.a1, self.a2, self.a3 = ResNet._forward(x, self.w)
+      return self.a3
+    else:
+      self.a1, self.a2 = ResNet._forward(x, self.w)
+      return self.a2
+
+  @staticmethod
+  def _forward(x, w):
+    a1 = sigmoid(w[0] @ x) + x
+    a2 = w[1] @ a1 + a1
+    if len(w) > 2:
+      a3 = w[2] @ sigmoid(a2) + a2
+      return a1, a2, a3
+    return a1, a2
+
+  def backward(self, x, y, lr):
+
+    dw = [None] * 2
+
+    if ADD_NOISE:
+      x = add_noise(x)
+
+    # w[2] will always be fixed, so we can "remove" it before calculating grads
+    if self.is_3_layer:
+      inv = np.linalg.inv(self.w[2])
+      y = inv_sigmoid(inv @ y)
+
+    error = self.a2 - y
+
+    dw[1] = error @ self.a1.T
+    dw[0] = self.w[1].T @ error * self.a1 * (1 - self.a1) @ x.T
+
+    # Only update weights for non-fixed parameters
+    pos = parameter_positions[WEIGHTS_DIST]
+    for _pos in pos:
+      self.w[_pos[0]][_pos[1], _pos[2]] -= lr * dw[_pos[0]][_pos[1], _pos[2]] / len(x)
+
+      # If the same parameter appears twice on the diagonal, fix them to be the same
+      if _pos[1:3] == (0, 1):
+        self.w[_pos[0]][1, 0] -= lr * dw[_pos[0]][1, 0] / len(x)
+
+        if WEIGHTS_DIST == 'skew':
+          self.w[_pos[0]][1, 0] = -self.w[_pos[0]][1, 0]
+
+        diff = self.w[_pos[0]][0, 1] - self.w[_pos[0]][1, 0]
+        self.w[_pos[0]][0, 1] -= diff / 2
+        self.w[_pos[0]][1, 0] += diff / 2
+
+        if WEIGHTS_DIST == 'skew':
+          self.w[_pos[0]][1, 0] = -self.w[_pos[0]][1, 0]
+
 def diag(a):
   return np.array([[a[0], a[1]], [a[1], a[2]]])
 
@@ -159,7 +218,7 @@ def form_weights(i, j, fixed):
     weights[pos[0][0]][pos[0][1] + pos[0][2]] = i
     weights[pos[1][0]][pos[1][1] + pos[1][2]] = j
 
-    if WEIGHTS_DIST == 'equal' or WEIGHTS_DIST == 'second':
+    if WEIGHTS_DIST in ['equal', 'second']:
       weights += [[fixed[6], fixed[7], fixed[8]]]
 
   return list(map(lambda x: diag(x), weights))
@@ -167,7 +226,9 @@ def form_weights(i, j, fixed):
 def add_noise(m):
   return m + np.random.normal(0, NOISE_STRENGTH, np.shape(m))
 
-def forward(x, w):
+def forward(x, w, net=None):
+  if net:
+    return net._forward(x, w)
   a1 = sigmoid(w[0] @ x)
   a2 = w[1] @ a1
   if len(w) > 2:
@@ -209,12 +270,12 @@ def train(epochs, m, X, Y, lr):
 
   return sgd_path, losses
 
-def calc_loss(i, j, fixed, Y):
-  y_hat = forward(X, form_weights(i, j, fixed))[-1]
+def calc_loss(i, j, fixed, Y, net):
+  y_hat = forward(X, form_weights(i, j, fixed), net)[-1]
   return loss(y_hat, Y)
 
-def create_landscape(axis, fixed, Y):
-  return [[calc_loss(i, j, fixed, Y) for i in axis] for j in axis]
+def create_landscape(axis, fixed, Y, net):
+  return [[calc_loss(i, j, fixed, Y, net) for i in axis] for j in axis]
 
 axis = np.arange(-AXIS_SIZE, AXIS_SIZE, AXIS_SIZE/100)
 
@@ -237,11 +298,11 @@ def plot(fixed, Y, paths=None):
 '''
   Plot 3D contours of the loss landscape
 '''
-def plot_3d(i, j, fixed, Y, paths=None):
+def plot_3d(i, j, fixed, Y, paths=None, net=None):
   fig = plt.figure()
   ax = fig.gca(projection='3d')
 
-  Z = np.array(create_landscape(axis, fixed, Y))
+  Z = np.array(create_landscape(axis, fixed, Y, net))
   axis_x, axis_y = np.meshgrid(axis, axis)
 
   ax.plot_surface(axis_x, axis_y, Z, cmap=cm.terrain, linewidth=0, alpha=0.7)
@@ -289,18 +350,25 @@ if __name__ == "__main__":
   # Model fixed weights set to same as Y, change to random to achieve non-convexity
   model_fixed = fixed if MODEL_FIXED_SAME else get_rand(12)
 
+  Net = None
+
   for _ in range(num_paths):
     rand_init = (np.random.rand(2) * 2 - 1) * AXIS_SIZE
 
     if WEIGHTS_DIST == 'rotational':
       model = RotationalNet(*rand_init)
     else:
-      model = TwoLayerNet(form_weights(*rand_init, model_fixed))
+      if WEIGHTS_DIST == 'resnet':
+        Net = ResNet
+      else:
+        Net = TwoLayerNet
+
+      model = Net(form_weights(*rand_init, model_fixed))
 
     path, losses = train(epochs, model, X, Y, lr)
     sgd_paths.append(path)
 
   # plot_losses(losses, epochs)
   # plot(model_fixed, Y, sgd_paths)
-  # plot_3d(rand[0], rand[1], model_fixed, Y)
-  plot_3d(rand[0], rand[1], model_fixed, Y, sgd_paths)
+  # plot_3d(rand[0], rand[1], model_fixed, Y, net=Net)
+  plot_3d(rand[0], rand[1], model_fixed, Y, sgd_paths, net=Net)
